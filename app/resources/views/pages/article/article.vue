b74a13724a21c75ef666e55b8590febfc12ee766
Adrian <mobistyle35@gmail.com>
2022-09-20 01:14:17 +0800

    new generation

---
+++ app/resources/views/pages/article/article.vue
@@ -0,0 +1,22 @@
+<template>
+<Head>
+  <title>this is an article page</title>
+  <meta name="description" content="Your page description">
+<meta property="og:url"           content="https://uplus.boragnitrealty.xyz" />
+<meta property="og:type"          content="store" />
+<meta property="og:title"         content="This is my custom titol" />
+<meta property="og:description"   :content="description" />
+<meta property="og:image"         content="https://www.your-domain.com/path/image.jpg" />
+</Head>
+  <div>
+    This is an article page
+  </div>
+
+</template>
+
+<script setup >
+  import { Link, Head } from "@inertiajs/inertia-vue3"
+import { ref } from "@vue/reactivity"
+
+  const description = ref('this description is really reactive')
+</script>
--- app/routes/web.php
+++ app/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return inertia('welcome');
+});
+
+Route::get('/article/article', function () {
+    return inertia('article/article');
 });
