2193b3c8f907ebd19257f66122b59a4e3ff7a90b
Adrian <mobistyle35@gmail.com>
2022-09-20 01:37:36 +0800

    Asd

--- app/config/inertia.php
+++ app/config/inertia.php
@@ -16,7 +16,7 @@ return [
     */
     'ssr' => [
         'enabled' => false,
-        'url' => 'http://127.0.0.1:13714/render',
+        'url' => 'https://127.0.0.1:13714/render',
     ],
     
     /*
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,4 @@
 import { defineConfig } from 'vite'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 import laravel from 'vite-plugin-laravel'
 import vue from '@vitejs/plugin-vue'
 import inertia from './resources/scripts/vite/inertia-layout'
